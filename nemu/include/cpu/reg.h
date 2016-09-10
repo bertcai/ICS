4ff864026831322cd47cf35c4892be6c82136893
tracer-ics2015 <tracer@njuics.org>
2016-09-10 10:16:03 +0800

    >  compile NEMU 161420111 caibowei Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2+deb8u3 (2016-07-02) i686 GNU/Linux  10:16:03 up  1:35,  2 users,  load average: 0.08, 0.03, 0.05 b5ec96c3b2cddf5df3ba45be2d043dcdee5aa4fb

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,20 +14,18 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-	struct {
+typedef union {
+	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	uint16_t ax, cx, dx, bx, sp, bp, si, di;
-	uint8_t al, cl, dl, bl, ah, ch, dh, bh;
-	swaddr_t eip;
-
+	struct{
+		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		swaddr_t eip;
+	};
 } CPU_state;
 
 extern CPU_state cpu;
